81135baeddb955e5ae9d481969383d67de9e3317
Moamen <moemena2002@gmail.com>
2024-12-19 21:06:11 +0200

    fixed the image but there are errors

--- app/components/navbar/nav.tsx
+++ app/components/navbar/nav.tsx
@@ -51,12 +51,10 @@ export function SidebarDemo() {
         "h-[60vh]" // for your use case, use `h-screen` instead of `h-[60vh]`
       )}
     >
-      <Sidebar open={open} setOpen={setOpen} animate={false}>
+      <Sidebar open={open} setOpen={setOpen}>
         <SidebarBody className="justify-between gap-10">
           <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-            <>
-              <Logo />
-            </>
+            {open ? <Logo /> : <LogoIcon />}
             <div className="mt-8 flex flex-col gap-2">
               {links.map((link, idx) => (
                 <SidebarLink key={idx} link={link} />
@@ -69,14 +67,20 @@ export function SidebarDemo() {
                 label: "Manu Arora",
                 href: "#",
                 icon: (
-                    <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+                  <Image
+                    src="https://assets.aceternity.com/manu.png"
+                    className="h-7 w-7 flex-shrink-0 rounded-full"
+                    width={50}
+                    height={50}
+                    alt="Avatar"
+                  />
                 ),
               }}
             />
           </div>
         </SidebarBody>
       </Sidebar>
-      {/* <Dashboard /> */}
+      <Dashboard />
     </div>
   );
 }
@@ -108,28 +112,28 @@ export const LogoIcon = () => {
   );
 };
 
-// // Dummy dashboard component with content
-// const Dashboard = () => {
-//   return (
-//     <div className="flex flex-1">
-//       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-//         <div className="flex gap-2">
-//           {[...new Array(4)].map((i) => (
-//             <div
-//               key={"first" + i}
-//               className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
-//             ></div>
-//           ))}
-//         </div>
-//         <div className="flex gap-2 flex-1">
-//           {[...new Array(2)].map((i) => (
-//             <div
-//               key={"second" + i}
-//               className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
-//             ></div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+// Dummy dashboard component with content
+const Dashboard = () => {
+  return (
+    <div className="flex flex-1">
+      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+        <div className="flex gap-2">
+          {[...new Array(4)].map((i) => (
+            <div
+              key={"first-array" + i}
+              className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
+            ></div>
+          ))}
+        </div>
+        <div className="flex gap-2 flex-1">
+          {[...new Array(2)].map((i) => (
+            <div
+              key={"second-array" + i}
+              className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
+            ></div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
